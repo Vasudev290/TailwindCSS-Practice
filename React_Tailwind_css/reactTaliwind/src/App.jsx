50d3fd5398b9becfd8b0e3daace7bd97a0b35841
Vasudev290 <vasukesavulu@gmail.com>
2025-01-18 13:49:37 +0530

    tailwind

--- React_Tailwind_css/reactTaliwind/src/App.jsx
+++ React_Tailwind_css/reactTaliwind/src/App.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h2 className='text-3xl font-bold underline border-8 border-red-900 p-4 bg-slate-500 my-1 mx-2 '>Hello World!</h2>
+    <div className='ml-10'>
+      <h2 className='text-3xl font-bold border-solid border-8 border-red-900 p-4 text-center bg-slate-500 m-1'>Hello World!</h2>
+      <h1>
+        <div className='border-2 border-red-800  w-96'>
+          Width Property..!
+        </div>
+      </h1>
     </div>
   )
 }
