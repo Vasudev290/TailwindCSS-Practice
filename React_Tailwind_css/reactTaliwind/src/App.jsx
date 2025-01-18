67a76a68b50b88f95e36257ec690acb779934f53
Vasudev290 <vasukesavulu@gmail.com>
2025-01-18 12:06:22 +0530

    tailwind

---
+++ React_Tailwind_css/reactTaliwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold underline border-8 border-red-900 p-4 bg-slate-500 my-1 mx-2 '>Hello World!</h2>
+    </div>
+  )
+}
+
+export default App
